1e5364a57946420ba6b7745ff4497a34f5acff6d
Krunoslav Lujic <python.developer.edukacija@gmail.com>
2024-03-05 19:12:28 +0100

    Lambda funkcije

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ def show_pin_panel():
     pin_panel.pack(padx=20, pady=10)
 
 
-def hide_pin_panel():
-    pin_panel.pack_forget()
-    pin_panel.tkraise() # opcija ako su okviri jedan ispod drugog kao karte
+def hide_pin_panel(panel):
+    panel.pack_forget() # Ako se koristi grid: button_panel.grid_remove(); Ako se koristi place: button_panel.place_forget()
+    panel.tkraise() # opcija ako su okviri jedan ispod drugog kao karte
 
 
 main_window = tk.Tk()
 main_window.title('Smart Key')
 main_window.geometry('600x1000')
+
+
 
 # Button Panel
 button_panel = tk.Frame(main_window)
@@ -24,7 +26,7 @@ lbl_welcome_message = tk.Label(button_panel,
 lbl_welcome_message.grid(row=0, column=0, columnspan=2)
 ring_buton = tk.Button(button_panel,
                        text='Pozvoni - sakrij',
-                       command=hide_pin_panel)
+                       command=lambda : hide_pin_panel(pin_panel))
 ring_buton.grid(row=1, column=0)
 unlock_buton = tk.Button(button_panel,
                        text='Otkljucaj',
@@ -33,12 +35,12 @@ unlock_buton.grid(row=1, column=1)
 
 
 
+
 # PIN Panel
 pin_panel = tk.Frame(main_window)
 lbl_pin = tk.Label(pin_panel,
                    text='Ovo je tekst unutar PIN labele')
 lbl_pin.grid(row=0, column=0)
-
 
 
 admin_panel = tk.Frame(main_window)
---
+++ py_lambda_funkcija.py
@@ -0,0 +1,6 @@
+def kvadriranje():
+    return 5 ** 2
+
+# lambda argumenti: izraz
+lambda : 5 ** 2
+lambda x, y: (x ** 2) + y
