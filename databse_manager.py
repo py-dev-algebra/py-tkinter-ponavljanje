b16f398113de25f4880a8da43c8735d214d0aca0
Krunoslav Lujic <python.developer.edukacija@gmail.com>
2024-03-05 20:35:28 +0100

    Finished

---
+++ databse_manager.py
@@ -0,0 +1,12 @@
+
+
+
+def get_names_from_db():
+    return ['Pero Peric', 'Ana Anic', 'Marko Maric', 'Iva Ivic', 'Vinko Vinic']
+    # TODO Presloziti da ova funkcija vrati objekte iz baze
+    # [f'{osoba.first_name} {osoba.last_name}', 'Ana Anic', 'Marko Maric', 'Iva Ivic', 'Vinko Vinic']
+
+
+def get_user(user_first_name):
+    pass
+    # TODO dohvatiti i vratiti objekt iz baze za osobu koja ima isto ime kao argument u funkciji
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-from tkinter import IntVar, BooleanVar
+from tkinter import IntVar, StringVar
 
+from databse_manager import get_names_from_db, get_user
 
 
 def show_pin_panel():
@@ -10,6 +11,19 @@ def show_pin_panel():
 def hide_pin_panel(panel):
     panel.pack_forget() # Ako se koristi grid: button_panel.grid_remove(); Ako se koristi place: button_panel.place_forget()
     panel.tkraise() # opcija ako su okviri jedan ispod drugog kao karte
+
+
+def leave(event):
+    print(f'Mis nije iznad gumba. {event}')
+
+
+def on_element_clicked(event):
+    index = lbox_names.curselection()
+    value = lbox_names.get(index)
+    user = get_user(value.split(' ')[0])
+    # TODO popuniti vrijednosti u widgetima preko Vars (StringVar, IntVar ...)
+
+    print(f'Index {index}; Value: {value}')
 
 
 main_window = tk.Tk()
@@ -40,12 +54,13 @@ unlock_buton.grid(row=1, column=1)
 #region PIN Panel
 pin_panel = tk.Frame(main_window)
 
-# CheckBox
+#region CheckBox
 cb_expand_var = IntVar()
 lbl_expand = tk.Label(pin_panel, text="Expand")
 lbl_expand.grid(row=0, column=0)
 cb_expand = tk.Checkbutton(pin_panel, variable=cb_expand_var)
 cb_expand.grid(row=0, column=1)
+#endregion
 
 
 lbl_pin = tk.Label(pin_panel,
@@ -59,13 +74,34 @@ entry_name = tk.Entry(pin_panel)
 entry_name.grid(row=2, column=1)
 
 
+#region ListBox
+lbox_names = tk.Listbox(pin_panel)
+lbox_names.grid(row=3,column=0)
+# lbox_names.insert(tk.END, 'Pero Peric')
+# lbox_names.insert(tk.END, 'Ana Anic')
 
-
+# names = ['Pero Peric', 'Ana Anic', 'Marko Maric', 'Iva Ivic', 'Vinko Vinic']
+names = get_names_from_db()
+for name in names:
+    lbox_names.insert(tk.END, name)
+lbox_names.bind('<ButtonRelease-1>', on_element_clicked)
 #endregion
 
 
+btn_demo = tk.Button(pin_panel,
+                     text='Demo',
+                     font=('Segoe UI', 15))
+btn_demo.grid(row=4, column=0, pady=20)
+btn_demo.bind('<Leave>', leave)
 
+lbl_demo_var = StringVar()
+lbl_demo_var.set('Pocetna vrijednost')
+lbl_demo = tk.Label(pin_panel,
+                    textvariable=lbl_demo_var,
+                    font=('Segoe UI', 15))
+lbl_demo.grid(row=5, column=0, pady=20)
 
+#endregion
 
 
 
@@ -74,4 +110,9 @@ admin_panel.pack(padx=20, pady=(10, 20))
 
 
 
-main_window.mainloop()
+
+if __name__ == '__main__':
+    # funkcija koja ce kreirati bazu i napuniti je inicijalnim podacima
+    # create_tables()
+    # database_seed()
+    main_window.mainloop()
